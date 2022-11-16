413532db022310b370f48032df65f23515c92d08
袁鹏 <rocyuan666@163.com>
2022-11-16 14:26:07 +0800

    fix

--- build/banner.js
+++ build/banner.js
@@ -7,7 +7,7 @@
 const { name, version, author, license } = require("../package.json");
 
 const banner = 
-`/*!
+`/*
   * echarts tooltip 自动显示轮播
   * ${name} v${version}
   * (c) 2022-${new Date().getFullYear()} ${author}
